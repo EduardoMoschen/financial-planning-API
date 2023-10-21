63fb311f534ff658aa1b931cf863ff9069e9ba1e
Eduardo Moschen <eduardo.moschen28@gmail.com>
2023-10-21 14:18:29 -0300

    feat: testes para operações relacionadas às categorias das transações e orçamentos
    testes da CategoryAPIList e CategoryAPIDetail contidas na views.py

---
+++ finances/tests/test_finances_category_detail_view.py
@@ -0,0 +1,110 @@
+from django.test import TestCase, RequestFactory
+from finances.views import CategoryAPIDetail
+from finances.models import Category, Account, Transaction, Budget
+from rest_framework import status
+from django.contrib.auth.models import User
+
+
+class CategoryAPIDetailTest(TestCase):
+    """
+    Teste para a classe CategoryAPIDetail.
+
+    Esta classe contém testes para os métodos da classe CategoryAPIDetail, que
+    lida com operações detalhadas relacionadas a uma categoria específica.
+    """
+
+    def setUp(self):
+        """
+        Configuração inicial para os testes.
+
+        Este método é executado antes de cada teste. Ele cria uma instância
+        inicial de objetos necessários para os testes.
+        """
+
+        self.factory = RequestFactory()
+
+        self.user = User.objects.create_user(
+            username='user1',
+            password='password1',
+            first_name='Carlos',
+            last_name='Alberto',
+            email='carlos@email.com'
+        )
+
+        self.account = Account.objects.create(
+            owner=self.user,
+            name='Conta Corrente',
+            balance=1000
+        )
+
+        self.category = Category.objects.create(
+            name='Academia'
+        )
+
+        self.transaction = Transaction.objects.create(
+            amount=200,
+            description='Pagamento de academia',
+            account=self.account,
+            category=self.category
+        )
+
+        self.budget = Budget.objects.create(
+            start_date='2023-08-01',
+            end_date='2023-08-31',
+            amount=150,
+            account=self.account,
+            category=self.category
+        )
+
+    def test_get(self):
+        """
+        Testa o método GET para obter uma categoria específica.
+
+        Este teste verifica se o método GET retorna o status HTTP 200 OK para
+        uma solicitação de listagem com detalhes de uma categoria válida.
+        """
+
+        view = CategoryAPIDetail.as_view()
+        request = self.factory.get(f'/api/category/{self.category.pk}/')
+        response = view(request, pk=self.category.pk)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_put(self):
+        """
+        Testa o método PUT para atualizar os dados de uma categoria específica.
+
+        Este teste verifica se o método PUT retorna o status HTTP 200 OK para
+        uma solicitação de atualização com os dados válidos da categoria.
+        """
+
+        view = CategoryAPIDetail.as_view()
+
+        new_data_category = {'name': 'Medicamentos'}
+
+        request = self.factory.put(
+            f'/api/category/{self.category.pk}/',
+            data=new_data_category,
+            content_type='application/json'
+        )
+
+        response = view(request, pk=self.category.pk)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete(self):
+        """
+        Testa o método DELETE para exclusão de uma categoria específica.
+
+        Este teste verifica se o método DELETE retorna o status HTTP 204 NO
+        CONTENT para uma solicitação de exclusão válida.
+        """
+
+        view = CategoryAPIDetail.as_view()
+        request = self.factory.delete(f'/api/category/{self.category.pk}/')
+        response = view(request, pk=self.category.pk)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Transaction.objects.filter(
+            category=self.category).exists()
+        )
+        self.assertFalse(Budget.objects.filter(
+            category=self.category).exists()
+        )
---
+++ finances/tests/test_finances_category_list_view.py
@@ -0,0 +1,116 @@
+from django.test import TestCase
+from finances.models import Category
+from finances.serializers import CategorySerializer
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+class CategoryAPIListTest(TestCase):
+    """
+    Testes para a API de CategoryAPIList.
+
+    Esta classe contém testes para os métodos da API de CategoryAPIList, que
+    lida com operações relacionadas às categorias que serão usadas nas
+    transações e orçamentos.
+    """
+
+    def setUp(self):
+        """
+        Configuração inicial para os testes.
+
+        Este método é executado antes de cada teste. Ele cria instâncias
+        iniciais de objetos necessários para os testes.
+        """
+
+        self.category_1 = Category.objects.create(
+            name='Alimentação'
+        )
+
+        self.category_2 = Category.objects.create(
+            name='Academia'
+        )
+
+        self.client = APIClient()
+
+    def test_get_categories(self):
+        """
+        Testa o método GET para obter todas as categorias cadastradas.
+
+        Este teste verifica se o método GET retorna todas as categorias com o
+        status HTTP 200 OK.
+        """
+
+        response = self.client.get('/api/categories/')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response['content-type'], 'application/json')
+
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+
+        self.assertEqual(response.data, serializer.data)
+
+    def test_no_categories(self):
+        """
+        Testa o método GET quando não há categorias cadastradas.
+
+        Este teste verifica se o método GET retorna a mensagem apropriada
+        quando não há categorias registradas.
+        """
+
+        Category.objects.all().delete()
+
+        response = self.client.get('/api/categories/')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data,
+            {'message': 'There are no registered categories.'}
+        )
+
+    def test_create_category(self):
+        """
+        Testa o método POST para criar uma nova conta.
+
+        Este teste verifica se o método POST cria uma nova categoria
+        corretamente e retorna o status HTTP 201 CREATED.
+        """
+
+        new_category_data = {
+            'name': 'Medicamentos'
+        }
+
+        response = self.client.post(
+            '/api/categories/',
+            data=new_category_data,
+            format='json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        created_category = Category.objects.get(name=new_category_data['name'])
+        self.assertEqual(created_category.name, new_category_data['name'])
+
+        serializer = CategorySerializer(created_category)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_create_category_with_invalid_data(self):
+        """
+        Testa o método POST para criar uma nova categoria com dados inválidos.
+
+        Este teste verifica se o método POST cria uma nova categoria com os
+        dados passados de forma inválida e retorna o status HTTP 400 BAD
+        REQUEST.
+        """
+
+        new_category_data = {
+            'name': ''
+        }
+
+        response = self.client.post(
+            '/api/categories/',
+            data=new_category_data,
+            format='json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
